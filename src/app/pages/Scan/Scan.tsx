87ab106c9aa2f0126c7f2bf96f7c2cfe2932c1a1
davidwobido <davidwobido@googlemail.com>
2021-11-09 19:36:34 +0100

    :lipstick: Style scan page

--- src/app/pages/Scan/Scan.tsx
+++ src/app/pages/Scan/Scan.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import ImageInput from '../../Components/ImageInput/ImageInput';
+import styles from './Scan.module.css';
 
 function Scan() {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
 
   return (
-    <div>
+    <div className={styles.wrapper}>
       <ImageInput onUpload={setImageUrl} />
-      <button disabled={imageUrl === null}>Scan</button>
-      <a href="#">Skip</a>
+      {/* This will be a single component  */}
+      <section className={styles.menubar}>
+        <button disabled={imageUrl === null}>Scan</button>
+        <a href="#" className={styles.button__inactive}>
+          Docs
+        </a>
+      </section>
     </div>
   );
 }
