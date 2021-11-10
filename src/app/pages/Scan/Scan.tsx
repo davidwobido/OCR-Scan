304412aaa5831740fd55d033305d4e0800c3e0ff
davidwobido <davidwobido@googlemail.com>
2021-11-10 14:05:36 +0100

    :sparkles: Add OCR scan function

--- src/app/pages/Scan/Scan.tsx
+++ src/app/pages/Scan/Scan.tsx
@@ -1,25 +1,54 @@
 import React, { useState } from 'react';
 import ImageInput from '../../Components/ImageInput/ImageInput';
 import styles from './Scan.module.css';
+import Tesseract from 'tesseract.js';
 
 function Scan() {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [recognizedText, setRecognizedText] = useState<string | null>(null);
+  const [progress, setProgress] = useState<number | null>(null);
+  const [status, setStatus] = useState<string | null>(null);
 
   return (
     <div className={styles.wrapper}>
       <h1>OCR Scan</h1>
-      <div className={styles.uploadwrapper}>
-        {imageUrl ? (
-          <img src={imageUrl} className={styles.image} />
-        ) : (
-          <img src="src/lib/Upload_icon.svg" />
-        )}
-        {/* Show the image as soon a new one is uploaded */}
-        <ImageInput onUpload={setImageUrl} />
+      {recognizedText ? (
+        <p>{recognizedText}</p>
+      ) : (
+        <div className={styles.uploadwrapper}>
+          {imageUrl ? (
+            <img src={imageUrl} className={styles.image} />
+          ) : (
+            <img src="src/lib/Upload_icon.svg" />
+          )}{' '}
+          <ImageInput onUpload={setImageUrl} />
+        </div>
+      )}
+      <div className={styles.progress}>
+        {progress ? <p>{progress * 100}%</p> : null}{' '}
+        {status ? <p>: {status}</p> : null}
       </div>
-      {/* This will be an extra component: */}
+
       <section className={styles.menubar}>
-        <button disabled={imageUrl === null}>Scan</button>
+        <button
+          disabled={imageUrl === null}
+          onClick={() => {
+            if (imageUrl) {
+              Tesseract.recognize(imageUrl, 'eng', {
+                logger: (message) => {
+                  console.log(message);
+                  setStatus(message.status);
+                  setProgress(message.progress);
+                },
+              }).then((result) => {
+                const text = result.data.text;
+                setRecognizedText(text);
+              });
+            }
+          }}
+        >
+          Scan
+        </button>
         <button className={styles.button__inactive}>Docs</button>
       </section>
     </div>
