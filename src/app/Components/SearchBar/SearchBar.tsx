9c82e522e3db20397b2a5f307e08ea35aa7ccb6e
davidwobido <davidwobido@googlemail.com>
2021-11-16 09:42:30 +0100

    :sparkles: Add search bar

---
+++ src/app/Components/SearchBar/SearchBar.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+
+type SearchInputProps = {
+  onSearch: (value: string) => void;
+};
+
+function SearchInput({ onSearch }: SearchInputProps): JSX.Element {
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    if (value.length === 0) {
+      return;
+    }
+
+    const timeoutId = setTimeout(() => {
+      onSearch(value);
+    }, 300);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value]);
+
+  return (
+    <input
+      value={value}
+      onChange={(event) => setValue(event.target.value)}
+      placeholder="Enter search"
+    />
+  );
+}
+
+export default SearchInput;
--- src/app/pages/DocList/DocList.tsx
+++ src/app/pages/DocList/DocList.tsx
@@ -18,16 +18,21 @@ function DocList(): JSX.Element {
 
   return (
     <section className={styles.wrapper}>
-      <h1 className={styles.pageTitle}>All Documents</h1>
-      <div className={styles.previewList}>
-        {documents.map((document) => (
-          <DocPreview
-            key={document.id}
-            title={document.title}
-            text={document.text}
-          />
-        ))}
-      </div>
+      <h1 className={styles.title}>All Documents</h1>
+      <section className={styles.searchBar}>search</section>
+      <section className={styles.content}>
+        {documents === null && <span>Loading …</span>}
+        {!documents && <span>No documents</span>}
+        <div className={styles.previewList}>
+          {documents.map((document) => (
+            <DocPreview
+              key={document.id}
+              title={document.title}
+              text={document.text}
+            />
+          ))}
+        </div>
+      </section>
       <section className={styles.menubar}>
         <button className={styles.button__inactive}>Home</button>
         <button className={styles.button__inactive}>Docs</button>
